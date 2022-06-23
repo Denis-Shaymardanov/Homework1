6efe1041aa912214f9a9b2b2cdaae44fd87e7a5d
Denis Shaymardanov <d9655352866@gmail.com>
2022-06-23 17:36:41 +0500

    added task 2

--- 002/Program.cs
+++ 002/Program.cs
@@ -2,3 +2,16 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("a = " + numberA + " b = " + numberB + " -> max = " + numberA);
+}
+else
+{
+    Console.WriteLine("a = " + numberA + " b = " + numberB + " -> max = " + numberB);
+}
+
